ffc7f4904a8814133af124318ee185dd551df4bf
JosZarate <pepe-ds-pokes@hotmail.com>
2020-01-18 12:48:51 -0600

    enemigo

---
+++ FlarryEvolution/Assets/Enemigos/jose/helicopteroC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class helicopteroC : MonoBehaviour
+{
+    public GameObject player;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
